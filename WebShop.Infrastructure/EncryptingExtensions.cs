bd1490a08403440b7ab126bd93cdc38a65b87d78
Tuduce Andrei <tuduce.andrei15@gmail.com>
2020-09-16 21:49:36 +0300

    update to use angular, rebuild from scrath the whole project

---
+++ WebShop.Infrastructure/EncryptingExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WebShop.Infrastructure
+{
+    public static class EncryptingExtensions
+    {
+
+        private static readonly string toSaltValue = "QwzWSdfAWdc12ZSSAWy";
+
+        public static string GetHashedString(this string str)
+        {
+            return Convert.ToBase64String(Hash(Encoding.UTF8.GetBytes(str), Encoding.UTF8.GetBytes(toSaltValue)));
+        }
+
+        private static byte[] Hash(byte[] password, byte[] salt)
+        {
+            byte[] saltedValue = password.Concat(salt).ToArray();
+
+            return new SHA256Managed().ComputeHash(saltedValue);
+        }
+    }
+}
